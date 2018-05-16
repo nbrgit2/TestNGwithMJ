5caea51ea5acd4e0d47d326bf60e51d3e3569f52
nbrgit2 <36083748+nbrgit2@users.noreply.github.com>
2018-05-16 15:58:50 +0530

    Initial commit

---
+++ src/com/generic/GenTest.java
@@ -0,0 +1,25 @@
+package com.generic;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+
+public class GenTest {
+  @Test
+  public void f() {
+	  
+	  
+  }
+  @BeforeMethod
+  public void beforeMethod() {
+	  
+	  
+  }
+
+  @AfterMethod
+  public void afterMethod() {
+	  
+	  
+  }
+
+}
