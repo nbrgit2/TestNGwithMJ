fbf227d315df151592c8c0e2307b92b253c58f2f
nbrgit2 <36083748+nbrgit2@users.noreply.github.com>
2018-07-13 18:13:36 +0530

    tetstsst

---
+++ src/com/modify2/Modify.java
@@ -0,0 +1,16 @@
+package com.modify2;
+
+
+import org.testng.annotations.Test;
+
+import com.generic.STKClass;
+
+public class Modify {
+	
+	
+  @Test
+  public void f() {
+	  
+	  STKClass.gen="Modified--22";
+  }
+}
